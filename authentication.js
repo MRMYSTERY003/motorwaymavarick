42260ee814cc9f167a4f2270213e7afb09d9bf84
aparsriram <112273564+aparsriram@users.noreply.github.com>
2023-12-20 13:05:13 +0530

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -2,7 +2,7 @@ import { auth, onAuthStateChanged } from './fbase.js';
 
 // Function to check authentication state and redirect to login if not authenticated
 export function checkAuthentication() {
-  onAuthStateChanged(auth, (user) => {
+  onAuthStateChanged(auth, (user)) => {
     if (!user) {
       // User is not authenticated, redirect to login page
       window.location = 'login.html';
