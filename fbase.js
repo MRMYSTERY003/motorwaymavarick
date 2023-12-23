d6655a1e8c93cf111c2849e5ed3859fe3434a910
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2023-12-23 13:58:14 +0530

    Add files via upload

--- fbase.js
+++ fbase.js
@@ -1,16 +1,16 @@
-import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js"; 
-import {getAuth,signInWithEmailAndPassword, createUserWithEmailAndPassword,  signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-auth.js";
-import { getDatabase,ref,set,child,onValue,update,remove, } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-auth.js";
+import { getDatabase, ref, set, child, onValue, update, remove, } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHCLZ7unC_5AV9WGbr0x4lHYHFGIq4-Xw",
-    authDomain: "motorwaymavericks-23929.firebaseapp.com",
-    databaseURL: "https://motorwaymavericks-23929-default-rtdb.firebaseio.com",
-    projectId: "motorwaymavericks-23929",
-    storageBucket: "motorwaymavericks-23929.appspot.com",
-    messagingSenderId: "175834495669",
-    appId: "1:175834495669:web:f9c4a037dcf72a4f275cfc"
-  };
+  apiKey: "AIzaSyC9bUuPZRj-sGX54fRYiVdyV5hAanDDR6Q",
+  authDomain: "motorwaymavericks-23929.firebaseapp.com",
+  databaseURL: "https://dummy-a8c5d-default-rtdb.firebaseio.com/",
+  projectId: "motorwaymavericks-23929",
+  storageBucket: "motorwaymavericks-23929.appspot.com",
+  messagingSenderId: "175834495669",
+  appId: "1:175834495669:web:f9c4a037dcf72a4f275cfc"
+};
 
 
 let app;
@@ -19,4 +19,4 @@ const database = getDatabase(app);
 const auth = getAuth();
 
 
-export {auth,database,ref,set,onValue,signInWithEmailAndPassword,child, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, onAuthStateChanged };
+export { auth, database, ref, set, onValue, signInWithEmailAndPassword, child, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, onAuthStateChanged };
